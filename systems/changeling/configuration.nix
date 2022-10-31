280b84aa5df0bf3dc3c2b7b6b187da6d3628e972
Philipp Böschen <catouc@philipp.boeschen.me>
2022-10-31 12:48:52 +0100

    Change hostname of changeling to changeling :)

--- systems/changeling/configuration.nix
+++ systems/changeling/configuration.nix
@@ -11,8 +11,8 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "changeling";
 
   networking.networkmanager.enable = true;
 
