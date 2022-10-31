d387ac41d0d66c71b8af514f9205724dcda8f02a
Philipp Böschen <catouc@philipp.boeschen.me>
2022-10-31 12:48:16 +0100

    Cleanup comments and formatting in changeling configuration

--- systems/changeling/configuration.nix
+++ systems/changeling/configuration.nix
@@ -5,12 +5,8 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
@@ -18,19 +14,11 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.utf8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "nl_NL.utf8";
     LC_IDENTIFICATION = "nl_NL.utf8";
@@ -43,14 +31,8 @@
     LC_TIME = "nl_NL.utf8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  
-  # Nvidia graphics
-  # services.xserver.videoDrivers = [ "nvidia" ];
-  # hardware.opengl.enable = true;
-
   # Enable the GNOME Desktop Environment.
+  services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.wayland = false;
@@ -81,31 +63,13 @@
     #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   virtualisation.docker.enable = true;
 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pboeschen = {
-    isNormalUser = true;
-    description = "Phil (Work)";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-  };
-  
   users.users.pb = {
     isNormalUser = true;
     description = "Phil (Prv)";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
   };
-
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = false;
-
-  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
 
   nix = {
     package = pkgs.nixFlakes;
@@ -114,8 +78,6 @@
     '';
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   programs.steam.enable = true;
 
   environment.systemPackages = with pkgs; [
@@ -129,28 +91,6 @@
     wget
     zip
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-  
-  # Enable GlobalProtect VPN
-  services.globalprotect.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
