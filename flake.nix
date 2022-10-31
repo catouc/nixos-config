6ba86d62d42a6db66e7f5569058eb7cdc3d223e2
Philipp Böschen <catouc@philipp.boeschen.me>
2022-10-31 12:00:15 +0100

    Working changeling configuration

--- flake.nix
+++ flake.nix
@@ -3,9 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -13,12 +18,33 @@
         config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
+      user = "pb";
     in {
       nixosConfigurations = {
         changeling = lib.nixosSystem {
           inherit system;
           modules = [
             ./systems/changeling/configuration.nix
+
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit user; };
+              home-manager.users.${user} = {
+                imports = [ ./home/home.nix ];
+              };
+            }
+          ];
+        };
+      };
+
+      homeManagerConfig = {
+        inherit system pkgs;
+        username = "pb";
+        homeDirectory = "/home/pb";
+        configuration = {
+          imports = [
+            ./home/home.nix
           ];
         };
       };
