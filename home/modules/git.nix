212b01fda276acf80f68673bb8b4566639092626
Philipp Böschen <catouc@philipp.boeschen.me>
2022-11-06 11:17:03 +0100

    Add delta git pager properly

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -10,6 +10,27 @@
     };
 
     extraConfig = {
+      core = {
+        pager = "delta";
+      };
+
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+
+      delta = {
+        navigate = true;
+        light = false;
+      };
+
+      merge = {
+        conflictstyle = "diff3";
+      };
+
+      diff = {
+        colorMoved = "default";
+      };
+
       init = {
         defaultBranch = "main";
       };
