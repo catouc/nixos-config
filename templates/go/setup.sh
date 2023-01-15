28be26cb3c9bc048a68c3b4e319b8b892c8bc32e
Philipp Böschen <catouc@philipp.boeschen.me>
2023-01-15 15:52:15 +0100

    Update Go template with more convenience

--- templates/go/setup.sh
+++ templates/go/setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 package_name="$1"
 
+echo "use flake" > .envrc
+direnv allow
 mv cmd/replace cmd/"$package_name"
-find . -type f -exec sed -i "s/replace/$package_name" {} \;
-
+find . -type f -exec sed -i "s/replace/$package_name/g" {} \;
