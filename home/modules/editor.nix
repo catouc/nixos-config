353c52f5fc8c122b8cd7591fbaf26e11e42c1f40
Philipp Böschen <philipp.boeschen@booking.com>
2023-08-15 13:42:01 +0200

    Add terraform-ls to neovim

--- home/modules/editor.nix
+++ home/modules/editor.nix
@@ -17,6 +17,7 @@
 	config = ''
 	  local lspconfig = require('lspconfig')
 	  lspconfig.gopls.setup{}
+	  lspconfig.terraformls.setup{}
           vim.api.nvim_create_autocmd('LspAttach', {
 	    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 	    callback = function(ev)
