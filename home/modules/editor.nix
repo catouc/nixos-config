e4269956d355c98b7d3a07d68fd94f51db404d8f
Philipp Böschen <philipp.boeschen@booking.com>
2023-08-23 12:16:26 +0200

    Add a hover for errors in neovim lsp

--- home/modules/editor.nix
+++ home/modules/editor.nix
@@ -25,6 +25,7 @@
               vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
               -- See `:help vim.lsp.*` for documentation on any of the below functions
               local opts = { buffer = ev.buf }
+	      vim.keymap.set('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
               vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
               vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
               vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
