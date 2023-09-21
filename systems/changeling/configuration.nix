e7900ec4087464f3fcd01acf39bdecef152ef0df
Philipp Böschen <catouc@philipp.boeschen.me>
2023-09-21 14:34:57 +0200

    Add ssh-agent and establish modules for systems

--- systems/changeling/configuration.nix
+++ systems/changeling/configuration.nix
@@ -5,7 +5,10 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ./hardware-configuration.nix];
+  imports = [
+    ./hardware-configuration.nix
+    ../modules/ssh.nix
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
