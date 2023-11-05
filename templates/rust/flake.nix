c61765c49c9732dfae40eb824a4e6b5a3e7fc4b2
Philipp Böschen <catouc@philipp.boeschen.me>
2023-11-05 11:21:54 +0100

    Add initial version of a rust template

---
+++ templates/rust/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+
+  description = "";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem ( system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        build = pkgs.rustPlatform.buildRustPackage {
+          pname = "replace";
+          version = "v0.1.0";
+          src = ./.;
+	  cargoSha256 = "";
+        };
+      in
+        rec {
+        packages = {
+          replace = build;
+          default = build;
+        };
+
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+	      cargo
+	      rustc
+	      rust-analyzer
+            ];
+          };
+        };
+      }
+    );
+}
+
