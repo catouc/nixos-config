2de21eb572e350911ab887f1dc10e2bbd5c94b36
Philipp Böschen <catouc@philipp.boeschen.me>
2023-11-05 14:17:24 +0100

    Remove terraformls from nvim because it seems like a hot mess

--- home/modules/editor.nix
+++ home/modules/editor.nix
@@ -22,7 +22,6 @@
 	    }
 	  }
 	  lspconfig.gopls.setup{}
-	  lspconfig.terraformls.setup{}
           vim.api.nvim_create_autocmd('LspAttach', {
 	    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
 	    callback = function(ev)
