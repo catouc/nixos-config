820edd69a8236bf36546095b8b08d675f9cab0cd
Philipp Böschen <catouc@philipp.boeschen.me>
2023-11-14 21:21:41 +0000

    Add hydra and nginx config to marut

--- systems/marut/configuration.nix
+++ systems/marut/configuration.nix
@@ -9,6 +9,11 @@
   zramSwap.enable = true;
   networking.hostName = "marut";
   networking.domain = "";
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 80 443 ];
+  };
+
   services.openssh.enable = true;
   programs.ssh.startAgent = true;
   environment.systemPackages = with pkgs; [
@@ -20,6 +25,34 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrXTWtqfBvZCn/SPlN0nZmhPhwvOc4M8gPeKN1b2eZ'' ];
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+    virtualHosts."hydra.catouc.com" = {
+      forceSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3000";
+        extraConfig = "proxy_ssl_server_name on;";
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "acme@philipp.boeschen.me";
+  };
+
+  services.hydra = {
+    enable = true;
+    hydraURL = "https://hydra.catouc.com";
+    listenHost = "127.0.0.1";
+    notificationSender = "hydra@catouc.com";
+    buildMachinesFiles = [];
+    useSubstitutes = true;
   };
 
   nix = {
