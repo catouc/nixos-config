1a5920ebbbfec91fb2298cb107140ae2a6bf1ff5
Philipp Böschen <catouc@philipp.boeschen.me>
2023-12-11 16:54:15 +0100

    Cleanup changeling config

--- systems/changeling/configuration.nix
+++ systems/changeling/configuration.nix
@@ -6,10 +6,9 @@
     ../modules/bluetooth.nix
     ../modules/boot.nix
     ../modules/docker.nix
-    ../modules/fonts.nix
     ../modules/hyprland.nix
     ../modules/jellyfin.nix
-    ../modules/locales.nix
+    ../modules/locale.nix
     ../modules/mullvad-vpn.nix
     ../modules/power.nix
     ../modules/printing.nix
@@ -18,13 +17,12 @@
     ../modules/steam.nix
     ../modules/user.nix
 
-    (import ../modules/1password.nix { username = "pboeschen"; })
+    (import ../modules/fonts.nix { pkgs = pkgs; })
     (import ../modules/nix.nix { pkgs = pkgs; })
     (import ../modules/network.nix {
       networkManagerEnabled = true;
       hostName = "changeling";
     })
-    (import ../modules/polkit-agent.nix { pkgs = pkgs; })
   ];
 
   pb.hyprland = {
