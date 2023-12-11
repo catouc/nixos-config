31b81edba8a5ab4405092a4afe24cb3643e7d664
Philipp Böschen <catouc@philipp.boeschen.me>
2023-12-11 16:53:42 +0100

    Moving szashune to my new module setup

--- systems/szashune/configuration.nix
+++ systems/szashune/configuration.nix
@@ -1,46 +1,25 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
-
 {
   imports = [
     ./hardware-configuration.nix
-    ../modules/jellyfin.nix
+
+    ../modules/boot.nix
+    ../modules/docker.nix
+    ../modules/fonts.nix
+    ../modules/hyprland.nix
+    ../modules/locale.nix
+    ../modules/printing.nix
     ../modules/ssh.nix
+    ../modules/sound.nix
+    ../modules/steam.nix
+    ../modules/user.nix
+
+    (import ../modules/nix.nix { pkgs = pkgs; })
+    (import ../modules/network.nix {
+      networkManagerEnabled = true;
+      hostName = "szashune";
+    })
   ];
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-  networking.hostName = "szashune"; # Define your hostname.
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Amsterdam";
-
-  i18n.defaultLocale = "en_GB.utf8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.utf8";
-    LC_IDENTIFICATION = "nl_NL.utf8";
-    LC_MEASUREMENT = "nl_NL.utf8";
-    LC_MONETARY = "nl_NL.utf8";
-    LC_NAME = "nl_NL.utf8";
-    LC_NUMERIC = "nl_NL.utf8";
-    LC_PAPER = "nl_NL.utf8";
-    LC_TELEPHONE = "nl_NL.utf8";
-    LC_TIME = "nl_NL.utf8";
-  };
-  
-  fonts = {
-    fontDir.enable = true;
-    fontconfig.enable = true;
-    enableDefaultPackages = true;
-    packages = [
-      pkgs.nerdfonts
-    ];
-  };
 
   fileSystems = {
     "/home/pb/.local/share/Steam" = {
@@ -49,79 +28,13 @@
     };
   };
 
-  services.xserver.enable = false;
-
-  environment.loginShellInit = ''
-    [[ "$(tty)" == /dev/tty1 ]] && WLR_NO_HARDWARE_CURSORS=1 Hyprland
-  '';
-
-  programs.hyprland = {
-    enable = true;
-    enableNvidiaPatches = true;
-  };
-
-  # Hardware Support for Wayland Sway
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-    };
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-      open = false;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-    bluetooth.enable = true;
-    pulseaudio.enable = false;
-  };
-
-  security.polkit.enable = true;
-
-  # Swaylock
-  security.pam.services.swaylock = {
-    text = "auth include login";
-  };
-
   # Nvidia graphics
+  # TODO: figure out if this can be safely dropped
   services.xserver.videoDrivers = [ "nvidia" ];
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "altgr-intl";
-  };
-
-  services.printing.enable = true;
-  services.upower.enable = true;
-
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
-  services.pipewire = {
+  pb.hyprland = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    wireplumber.enable = true;
+    nvidiaGPU = true;
   };
 
-  virtualisation.docker.enable = true;
-
-  users.users.pb = {
-    isNormalUser = true;
-    description = "Phil (Prv)";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-  };
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  programs.steam.enable = true;
   system.stateVersion = "22.05";
 }
