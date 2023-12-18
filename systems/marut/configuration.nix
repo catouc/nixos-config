e63c2ec9a75ee7e9c3a095c01642d7a3598a83a3
Philipp Böschen <catouc@philipp.boeschen.me>
2023-12-18 03:00:09 +0100

    Move around things in the marut config to make more sense

--- systems/marut/configuration.nix
+++ systems/marut/configuration.nix
@@ -5,6 +5,7 @@
 
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
+
   networking.hostName = "marut";
   networking.domain = "";
   networking.firewall = {
@@ -12,27 +13,29 @@
     allowedTCPPorts = [ 80 443 ];
   };
 
-  services.openssh.enable = true;
-  programs.ssh.startAgent = true;
-  environment.systemPackages = with pkgs; [
-    git
-    vim
-  ];
-  users.users.root.openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrXTWtqfBvZCn/SPlN0nZmhPhwvOc4M8gPeKN1b2eZ'' ];
-
-  users.groups.ytdl-sub = {};
-
-  users.users.pb = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "downloaders" ];
-    openssh.authorizedKeys.keys = [
-      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrXTWtqfBvZCn/SPlN0nZmhPhwvOc4M8gPeKN1b2eZ''
-    ];
+  users.groups = {
+    ytdl-sub = {};
   };
 
-  users.users.ytdl-sub = {
-    isSystemUser = true;
-    group = "ytdl-sub";
+  users.users = {
+    root = {
+      openssh.authorizedKeys.keys = [
+        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrXTWtqfBvZCn/SPlN0nZmhPhwvOc4M8gPeKN1b2eZ''
+      ];
+    };
+
+    pb = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [
+        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUrXTWtqfBvZCn/SPlN0nZmhPhwvOc4M8gPeKN1b2eZ''
+      ];
+    };
+
+    ytdl-sub = {
+      isSystemUser = true;
+      group = "ytdl-sub";
+    };
   };
 
   fileSystems = {
@@ -64,6 +67,9 @@
     };
   };
 
+  services.openssh.enable = true;
+  services.jellyfin.enable = true;
+
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
@@ -87,13 +93,6 @@
     };
   };
 
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "acme@philipp.boeschen.me";
-  };
-
-  services.jellyfin.enable = true;
-
   services.hydra = {
     enable = true;
     hydraURL = "https://hydra.catouc.com";
@@ -103,12 +102,22 @@
     useSubstitutes = true;
   };
 
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "acme@philipp.boeschen.me";
+  };
+
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
   };
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+  ];
 
   system.stateVersion = "23.05";
 }
