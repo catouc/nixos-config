694084b0dbe1030eb99ca48a366440f097dc64bb
Philipp Böschen <catouc@philipp.boeschen.me>
2024-02-07 09:39:01 +0100

    Move alacritty config to toml

--- home/modules/terminal.nix
+++ home/modules/terminal.nix
@@ -6,7 +6,7 @@
 
   home.file."alacritty.yml" = {
     enable = true;
-    source = ../configs/alacritty.yml;
-    target = "./.config/alacritty/alacritty.yml";
+    source = ../configs/alacritty.toml;
+    target = "./.config/alacritty/alacritty.toml";
   };
 }
