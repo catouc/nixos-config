dc8a5ba7ff5666633fdcb09deec28a54b39b6a97
Philipp Böschen <catouc@philipp.boeschen.me>
2024-02-20 11:28:12 +0100

    Remove NetworkManager-wait-online systemd oneshot
    This improves my boot times by 5 seconds and doesn't seem to impact anything else

--- systems/modules/boot.nix
+++ systems/modules/boot.nix
@@ -4,4 +4,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Oneshot service that only waits for the network to be available
+  # all of my installs come from local disk so this is unnecessarily
+  # delaying my boot
+  systemd.services.NetworkManager-wait-online.enable = false;
 }
