c6522fb5aed7ee5e0ea4aad0be7b41b30128f5ab
Philipp Böschen <catouc@philipp.boeschen.me>
2024-05-06 17:14:57 +0200

    Add a specialisation for Lix
    https://lix.systems/ -> testing it out, so far no issues

--- systems/szashune/configuration.nix
+++ systems/szashune/configuration.nix
@@ -19,6 +19,35 @@
     })
   ];
 
+  specialisation = {
+    lix.configuration = {
+      system.nixos.tags = [ "lix" ];
+
+      nix.settings.extra-substituters = [
+        "https://cache.lix.systems"
+      ];
+
+      nix.settings.trusted-public-keys = [
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      ];
+
+      imports = [
+        (import
+          (
+            (fetchTarball { url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz"; sha256 = "1qpgq280lsfiafq2sha8ln8jv7cdn4lfp3pjij9bqsmf369wdns3"; }) + "/module.nix"
+          )
+          {
+            lix = fetchTarball { url = "https://git.lix.systems/lix-project/lix/archive/2.90-beta.1.tar.gz"; sha256 = "0d0dbn08pmrf7zlkihiyagcpy9ywdr14r6brqrjg47aqcjisaia4"; };
+          }
+        )
+      ];
+    };
+
+    nix.configuration = {
+      system.nixos.tags = [ "nix" ];
+    };
+  };
+
   networking.nftables.enable = true;
   networking.nftables.flushRuleset = true;
 
