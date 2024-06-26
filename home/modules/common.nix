fcbb57d9151132fbaa61ab962efce7665cc6967d
Philipp Böschen <catouc@philipp.boeschen.me>
2024-06-26 15:47:23 +0200

    Cleanup common packages installed on my machines

--- home/modules/common.nix
+++ home/modules/common.nix
@@ -1,21 +1,4 @@
 { pkgs, ... }:
-let
-  todo = pkgs.writeShellScriptBin "todo" ''
-    #! /usr/bin/env bash
-    set -uo pipefail
-
-    touch ~/todo
-
-    CURRENT_DATE=$(date --rfc-3339=date)
-    grep "$CURRENT_DATE" ~/todo
-    if [ "$?" -ne 0 ]; then
-      echo "$CURRENT_DATE" >> ~/todo
-    fi
-
-    echo "$(date --rfc-3339 seconds): " >> ~/todo
-    vim + ~/todo
-  '';
-in
 {
   home.packages = with pkgs; [
     bluez
@@ -23,21 +6,18 @@ in
     dig
     file
     firefox
-    gcc
     git
-    google-chrome
     htop
     jq
-    ncspot
     powertop
     ripgrep
     semver
     tmux
-    todo
     unzip
     vim
     wget
     xsv
+    xclip
     zip
   ];
 
