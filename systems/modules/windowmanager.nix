2fe93a70411935842f6dd330d2d77d9a7586993b
Philipp Böschen <catouc@philipp.boeschen.me>
2024-07-14 11:05:52 +0200

    Add brightnessctl to properly control screenbrightness

--- systems/modules/windowmanager.nix
+++ systems/modules/windowmanager.nix
@@ -27,6 +27,7 @@ in
         enable = true;
         package = pkgs.i3-gaps;
         extraPackages = with pkgs; [
+          brightnessctl
           dmenu #application launcher most people use
           i3status # gives you the default i3 status bar
           i3lock #default i3 screen locker
