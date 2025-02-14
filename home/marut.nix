980cf4f00a55fdcea1c991ca31e340ea88eaa05d
Philipp Böschen <catouc@philipp.boeschen.me>
2025-02-14 13:28:02 +0100

    Clean up unused things for marut

--- home/marut.nix
+++ home/marut.nix
@@ -1,20 +1,8 @@
 { pkgs, config, ... }:
 {
-  imports = [
-    (import ./modules/git.nix {
-      git-email = "catouc@philipp.boeschen.me";
-    })
-  ];
-
   home = {
     username = "pb";
     homeDirectory = "/home/pb";
     stateVersion = "23.05";
   };
-
-  home.packages = with pkgs; [
-    ripgrep
-    ytdl-sub
-    rtorrent
-  ];
 }
