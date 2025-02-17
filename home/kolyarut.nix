bdc1d47e75a6f719829a24a10362aad28772b09e
Philipp Böschen <catouc@philipp.boeschen.me>
2025-02-17 18:47:21 +0100

    Add kolyarut!

---
+++ home/kolyarut.nix
@@ -0,0 +1,36 @@
+{ pkgs, config, ... }:
+{
+  imports = [
+    ./modules/i3.nix
+    ./modules/terminal.nix
+    ./modules/git.nix
+  ];
+
+  home = {
+    username = "pb";
+    homeDirectory = "/home/pb";
+    stateVersion = "22.05";
+    packages = with pkgs; [
+      mpv
+      thunderbird
+    ];
+  };
+
+  pb.home.terminal = {
+    enable = true;
+  };
+
+  pb.home.i3 = {
+    enable = true;
+    configFile = ./configs/kolyarut-i3;
+  };
+
+  pb.home.git = {
+    enable = true;
+    email = "catouc@philipp.boeschen.me";
+    urlRewrites = [{
+      from = "https://github.com";
+      to = "ssh://git@github.com";
+    }];
+  };
+}
---
+++ systems/kolyarut/configuration.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../modules/boot.nix
+    ../modules/locale.nix
+    ../modules/mullvad.nix
+    ../modules/security.nix
+    ../modules/sound.nix
+    ../modules/user.nix
+    ../modules/windowmanager.nix
+
+    (import ../modules/fonts.nix { pkgs = pkgs; })
+    (import ../modules/nix.nix { pkgs = pkgs; })
+    (import ../modules/network.nix {
+      networkManagerEnabled = true;
+      hostName = "kolyarut";
+    })
+  ];
+
+  networking.nftables.enable = true;
+  networking.nftables.flushRuleset = true;
+
+  pb.windowmanager = {
+    enable = true;
+    configFile = ../../home/configs/kolyarut-i3;
+  };
+
+  pb.locale.enable = true;
+  pb.mullvad = {
+    enable = true;
+    portBypasses = [ 22 ];
+  };
+  networking.wireguard.enable = true;
+
+  programs.ssh.startAgent = true;
+  programs.steam.enable = true;
+
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+  services.upower.enable = true;
+
+  virtualisation.containers.enable = true;
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+
+  system.stateVersion = "22.05";
+}
