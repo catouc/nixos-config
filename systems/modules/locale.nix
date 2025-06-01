2a3e476ef3b02e6d6d8d50b9511fe0b0b11fa1d5
Philipp Böschen <catouc@philipp.boeschen.me>
2025-06-01 14:48:47 +0200

    Update flake.lock
    This came with a breaking change on glibc-locales that now was upset
    that my locale names where wrong, so I fixed them. Note that the error
    message suggests something like:
    
    ```
    zh_CN.GBK/GBK \
    zh_CN.UTF-8/UTF-8 \
    zh_CN/GB2312 \
    zh_HK.UTF-8/UTF-8 \
    zh_HK/BIG5-HKSCS \
    zh_SG.GBK/GBK \
    zh_SG.UTF-8/UTF-8 \
    zh_SG/GB2312 \
    zh_TW.EUC-TW/EUC-TW \
    zh_TW.UTF-8/UTF-8 \
    zh_TW/BIG5 \
    zu_ZA.UTF-8/UTF-8 \
    zu_ZA/ISO-8859-1 \
    Error: unsupported locales detected:
    en_GB.utf8/UTF-8 \
    You should choose from the list above the error.
    ```
    
    While the nix code for locales adds the `/UTF-8` silently to the end of
    the string, that's the diff between the two.

--- systems/modules/locale.nix
+++ systems/modules/locale.nix
@@ -9,17 +9,17 @@ in
   };
   config = mkIf cfg.enable {
     time.timeZone = "Europe/Amsterdam";
-    i18n.defaultLocale = "en_GB.utf8";
+    i18n.defaultLocale = "en_GB.UTF-8";
     i18n.extraLocaleSettings = {
-      LC_ADDRESS = "en_GB.utf8";
-      LC_IDENTIFICATION = "en_GB.utf8";
-      LC_MEASUREMENT = "en_GB.utf8";
-      LC_MONETARY = "nl_NL.utf8";
-      LC_NAME = "en_GB.utf8";
-      LC_NUMERIC = "de_DE.utf8";
-      LC_PAPER = "en_GB.utf8";
-      LC_TELEPHONE = "en_GB.utf8";
-      LC_TIME = "en_GB.utf8";
+      LC_ADDRESS = "en_GB.UTF-8";
+      LC_IDENTIFICATION = "en_GB.UTF-8";
+      LC_MEASUREMENT = "en_GB.UTF-8";
+      LC_MONETARY = "nl_NL.UTF-8";
+      LC_NAME = "en_GB.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "en_GB.UTF-8";
+      LC_TELEPHONE = "en_GB.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
     };
 
     # TODO: I have no idea if this works if the xserver is disabled?!
