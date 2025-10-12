4005a61353ae0f2465cd84ae7981bbef0af9d664
Philipp Böschen <catouc@philipp.boeschen.me>
2025-10-12 20:07:13 +0200

    Automate gc and store optimisation

--- systems/modules/nix.nix
+++ systems/modules/nix.nix
@@ -1,6 +1,12 @@
 { ... }:
 {
   nix = {
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 15d";
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
