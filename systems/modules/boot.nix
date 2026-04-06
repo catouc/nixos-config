cc601f8715af9fcabaec0542c3653dd1db21b19e
Philipp Böschen <catouc@philipp.boeschen.me>
2026-04-06 21:17:21 +0200

    Optimise boot times by removing nix-gc from the immediate boot
    https://discourse.nixos.org/t/nixos-boots-really-slowly/28185/4
    
    ```
    > sudo systemd-analyze blame
    19.946s nix-gc.service
      643ms NetworkManager.service
      596ms home-manager-pb.service
      486ms nftables.service
    ...
    ```

--- systems/modules/boot.nix
+++ systems/modules/boot.nix
@@ -4,6 +4,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # This thing is exceptionally slow at boot
+  nix.gc.randomizedDelaySec = "2m";
+
   # Oneshot service that only waits for the network to be available
   # all of my installs come from local disk so this is unnecessarily
   # delaying my boot
