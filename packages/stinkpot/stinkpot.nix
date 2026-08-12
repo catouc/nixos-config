8e453a0836e2d0f8c5bfa0dc5f8f56ff1bbab60c
Philipp Böschen <catouc@philipp.boeschen.me>
2026-08-12 11:01:20 +0200

    Pin stinkpot to a commit hash
    The author seems to not believe in git tags :)

--- packages/stinkpot/stinkpot.nix
+++ packages/stinkpot/stinkpot.nix
@@ -7,8 +7,8 @@ buildGoModule {
   pname      = "stinkpot";
   version    = "0.1.0";
   src        = fetchTarball {
-    url="https://tangled.org/oppi.li/stinkpot/archive/main?format=tar.gz";
-    sha256="sha256-65QVLKVRGIPSCBYDemGqqOBXvMWxvo0ms65bCaw9Bfg=";
+    url="https://tangled.org/oppi.li/stinkpot/archive/8fa6de51adebb1ddeffbfb3b79c0885c2403575a?format=tar.gz";
+    sha256="sha256-766l6US0ISPPO7ygPtYryInvLF9wF0q1fWc9IWlSGVY=";
   };
-  vendorHash = "sha256-iDlU/176inkilehXft25KjiLt7rUtlMGqod22A3O/ko=";
+  vendorHash = "sha256-IVPACl1oWnBKGzcXvG5gzev8MwhzIKNI7zwEKJjhFc8=";
 }
